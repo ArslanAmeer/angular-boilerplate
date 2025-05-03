bda75ec46b5d7912f221a71ed9fbed3390942dd9
localghost1234 <viczaz99@hotmail.com>
2025-05-03 11:32:16 -0700

    improved nav menu's styling; deleted usage of HeaderComponent

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -8,12 +8,11 @@ import { ShellComponent } from './shell.component';
 import { HumanizePipe } from '@shared/pipes';
 import { FormsModule } from '@angular/forms';
 import { NavMenuComponent } from '@app/shell/components/navmenu/navmenu.component';
-import { HeaderComponent } from '@app/shell/components/header/header.component';
 import { PagesModule } from '@pages/pages.module';
 import { LanguageSelectorComponent } from '@app/i18n';
 
 @NgModule({
   imports: [CommonModule, TranslateModule, AuthModule, RouterModule, HumanizePipe, FormsModule, PagesModule, LanguageSelectorComponent],
-  declarations: [ShellComponent, HeaderComponent, NavMenuComponent],
+  declarations: [ShellComponent, NavMenuComponent],
 })
 export class ShellModule {}
